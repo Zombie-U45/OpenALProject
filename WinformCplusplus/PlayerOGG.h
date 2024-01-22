3f3cf179d1131e5e1fbc90007250b71e317f8bba
Yazami <86060998+Yazamoule@users.noreply.github.com>
2024-01-22 17:01:34 +0100

    playerOGG
    c'est une copie de player pour le momant

---
+++ WinformCplusplus/PlayerOGG.h
@@ -0,0 +1,11 @@
+
+#pragma once
+
+int PlayOGG(const char* fileName);
+int StopOGG(const char* fileName);
+int PauseOGG(const char* fileName);
+int ResumeOGG(const char* fileName);
+int initOGG();
+
+
+
