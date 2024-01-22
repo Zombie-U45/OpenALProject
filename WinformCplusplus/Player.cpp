c1a94d1d0242febae2603b3cc89f9d9adbd3509d
Aesma <115998345+alicia-Aesma@users.noreply.github.com>
2024-01-22 16:35:32 +0100

    commit button stop(partiellemnt fonctionnel)

--- WinformCplusplus/Player.cpp
+++ WinformCplusplus/Player.cpp
@@ -47,8 +47,31 @@ int Play(const char * fileName)
 	} while (iState == AL_PLAYING);
 	*/
 
+	return 0;
+}
+
+int Stop(const char* fileName)
+{
+	ALuint      uiBuffer;
+	ALuint      uiSource;
+
+	// Generate an AL Buffer
+	alGenBuffers(1, &uiBuffer);
+
+	// Load Wave file into OpenAL Buffer
+	if (!ALFWLoadWaveToBuffer((char*)fileName, uiBuffer))
+	{
+		ALFWprintf("Failed to load %s\n", fileName);
+	}
+
+	// Generate a Source to playback the Buffer
+	alGenSources(1, &uiSource);
+
+	// Attach Source to Buffer
+	alSourcei(uiSource, AL_BUFFER, uiBuffer);
+
 	// Clean up by deleting Source(s) and Buffer(s)
-	/*
+	
 	alSourceStop(uiSource);
 	alDeleteSources(1, &uiSource);
 	alDeleteBuffers(1, &uiBuffer);
@@ -56,6 +79,16 @@ int Play(const char * fileName)
 	ALFWShutdownOpenAL();
 
 	ALFWShutdown();
-	*/
+	
+	return 0;
+}
+
+int Pause(const char* fileName)
+{
+	return 0;
+}
+
+int Resume(const char* fileName)
+{
 	return 0;
 }
--- WinformCplusplus/Player.h
+++ WinformCplusplus/Player.h
@@ -2,6 +2,9 @@
 #pragma once
 
 	int Play(const char * fileName);
+	int Stop(const char* fileName);
+	int Pause(const char* fileName);
+	int Resume(const char* fileName);
 	int init();
 
 
